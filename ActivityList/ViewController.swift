3d82c9eaf8c5b05d303798d4169aaa8c2285f6d0
Alfredo Luco <aluco@alumnos.uai.cl>
2015-06-23 22:56:14 -0400

    ActivityList.xcscheme

--- ActivityList/ViewController.swift
+++ ActivityList/ViewController.swift
@@ -8,18 +8,110 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var busqueda: UISearchBar!
+    @IBOutlet weak var TableView: UITableView!
+    @IBOutlet weak var AddText: UITextField!
+    var ActivityList: [String] = []
+    var filtered: [String] = []
+    var searchActive: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        TableView.dataSource = self
+        AddText.hidden = true
+        TableView.delegate = self
+        busqueda.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func AddActivity(sender: UIBarButtonItem) {
+        if(!AddText.hidden){
+            var lastActivity: UILabel! = UILabel()
+            if var label : UILabel! = lastActivity{
+                label.text = AddText.text
+            }
+            ActivityList.append(lastActivity.text!)
+            println(lastActivity.text)
+            println(ActivityList)
+            //agregar a la tabla y actualizar
+            self.TableView.reloadData()
+            //self.TableView.reloadData()
+            AddText.text = ""
+            AddText.hidden = true
+        }else{
+            AddText.hidden = false
+        }
+    }
+    //Actualizar el ViewTable
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (searchActive){
+            return filtered.count
+        }
+        if (!ActivityList.isEmpty){
+            return ActivityList.count
+        }else{
+            return 0
+        }
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "k\(indexPath)")
+        
+        //cell.addSubview(ActivityList[indexPath.row] as! UIView)
+        if (searchActive){
+            cell.textLabel?.text = filtered[indexPath.row]
+        }else{
+            cell.textLabel?.text = ActivityList[indexPath.row]
+        }
+        println("Celda Agregada")
+        println(cell.textLabel)
+        
+        return cell
+    }
+    
+    //Motor de busqueda
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filtered = self.ActivityList.filter({ (text: String) -> Bool in
+            let tmp: NSString = text
+            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        self.TableView.reloadData()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
     }
 
-
+    //motor de eliminacion
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            ActivityList.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
 }
 
